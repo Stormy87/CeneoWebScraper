b2144f4f229b1ad7184014c56cb4490e246cff65
Stormy87 <tomek.jarzabek87@gmail.com>
2025-05-06 12:27:19 +0200

    12

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import os
+import json
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, send_file
 
 @app.route('/')
 def index():
@@ -14,9 +16,22 @@ def extract_post():
     product_id = request.form.get('product_id')
     return redirect(url_for('product', product_id=product_id))
 
+# @app.route('/products')
+# def products():
+#     products = os.listdir("./app/data/products")
+#     return render_template("products.html", products=products)
+
 @app.route('/products')
 def products():
-    return render_template("products.html")
+  products_dir = "app/data/products"
+  products = []
+  for filename in os.listdir(products_dir):
+    if filename.endswith('.json'):
+      file_path = os.path.join(products_dir, filename)
+      with open(file_path, 'r', encoding='utf-8') as file:
+        product = json.load(file)  
+        products.append(product)  
+  return render_template('products.html', products=products)
 
 @app.route('/author')
 def author():
